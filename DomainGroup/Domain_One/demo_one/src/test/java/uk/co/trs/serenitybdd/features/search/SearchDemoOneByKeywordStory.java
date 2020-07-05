eae326b8a2a218ca58be52ff2e19f0d2ec61fb83
Terry Strachan <AbstractReference@gmail.com>
2020-07-05 13:51:35 +0100

    initial commit

---
+++ DomainGroup/Domain_One/demo_one/src/test/java/uk/co/trs/serenitybdd/features/search/SearchDemoOneByKeywordStory.java
@@ -0,0 +1,48 @@
+package uk.co.trs.serenitybdd.features.search;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.questions.page.TheWebPage;
+import net.thucydides.core.annotations.Issue;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import uk.co.trs.serenitybdd.tasks.OpenTheApplication;
+import uk.co.trs.serenitybdd.tasks.Search;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.*;
+import static net.serenitybdd.screenplay.EventualConsequence.eventually;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+
+@RunWith(SerenityRunner.class)
+public class SearchDemoOneByKeywordStory {
+
+    Actor anna = Actor.named("Anna Demo1");
+
+    @Managed(uniqueSession = true)
+    public WebDriver herBrowser;
+
+    @Steps
+    OpenTheApplication openTheApplication;
+
+    @Before
+    public void annaCanBrowseTheWeb() {
+        anna.can(BrowseTheWeb.with(herBrowser));
+    }
+
+    @Test
+    public void demo1_search_results_should_show_the_search_term_in_the_title() {
+
+        givenThat(anna).wasAbleTo(openTheApplication);
+
+        when(anna).attemptsTo(Search.forTheTerm("Demo1 In Action"));
+
+        then(anna).should(eventually(seeThat(TheWebPage.title(), containsString("Demo1 In Action"))));
+
+    }
+}
